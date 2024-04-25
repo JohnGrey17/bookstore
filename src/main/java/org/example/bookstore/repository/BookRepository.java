a0fd1fa59ec87a8d9b251c7aa2104280c60ba4e6
John <pandastars535@gmail.com>
2024-04-25 20:53:16 +0300

    removed BookRepository implementation Class instead that did extend JpaRepositoty Interface

--- src/main/java/org/example/bookstore/repository/BookRepository.java
+++ src/main/java/org/example/bookstore/repository/BookRepository.java
@@ -2,8 +2,9 @@ package org.example.bookstore.repository;
 
 import java.util.List;
 import org.example.bookstore.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository {
+public interface BookRepository extends JpaRepository<Book,Long> {
     Book save(Book book);
 
     List<Book> findAll();
