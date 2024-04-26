7d8e65fd4937a48b617172c8de367873f99815e7
John <pandastars535@gmail.com>
2024-04-26 19:42:34 +0300

    Added BookSearchParameter class to DTO folder added specificationBuilderInterface added bookSpecificationBuilderImpl(without implementation)

--- src/main/java/org/example/bookstore/repository/BookRepository.java
+++ src/main/java/org/example/bookstore/repository/BookRepository.java
@@ -2,7 +2,8 @@ package org.example.bookstore.repository;
 
 import org.example.bookstore.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface BookRepository extends JpaRepository<Book,Long> {
+public interface BookRepository extends JpaRepository<Book,Long>, JpaSpecificationExecutor<Book> {
 
 }
