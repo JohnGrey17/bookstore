27586ea36382ab63349131fef66416d546e67e67
John <pandastars535@gmail.com>
2024-05-06 17:19:13 +0300

    deleted InvalidPriceException deleted annotation @Digit for BigInteger variable

--- src/main/java/org/example/bookstore/dto/BookRequestDto.java
+++ src/main/java/org/example/bookstore/dto/BookRequestDto.java
@@ -1,7 +1,6 @@
 package org.example.bookstore.dto;
 
 import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
 import java.math.BigDecimal;
 import lombok.Data;
@@ -15,7 +14,6 @@ public class BookRequestDto {
     @NotBlank
     private String isbn;
     @DecimalMin(value = "0.0", inclusive = false)
-    @Digits(integer = 3, fraction = 2)
     private BigDecimal price;
     private String description;
     private String coverImage;
