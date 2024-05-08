6cdee93abf1008988edd24ec1b7e4898ec29554a
John <pandastars535@gmail.com>
2024-05-08 23:51:41 +0300

    implemented UserRegistration Service Added Registration Exception added Handler for Registration Exception Added common path for all EndPoints

---
+++ src/main/java/org/example/bookstore/service/impl/user/UserServiceImpl.java
@@ -0,0 +1,31 @@
+package org.example.bookstore.service.impl.user;
+
+import lombok.RequiredArgsConstructor;
+import org.example.bookstore.dto.userdto.UserRegistrationRequestDto;
+import org.example.bookstore.dto.userdto.UserResponseDto;
+import org.example.bookstore.exception.RegistrationException;
+import org.example.bookstore.mapper.UserMapper;
+import org.example.bookstore.model.User;
+import org.example.bookstore.repository.user.UserRepository;
+import org.example.bookstore.service.user.UserService;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    @Override
+    public UserResponseDto register(UserRegistrationRequestDto requestDto)
+             throws RegistrationException {
+
+        if (userRepository.existsByEmail(requestDto.getEmail())) {
+            throw new RegistrationException("User with that email: "
+                    + requestDto.getEmail() + " is already exist");
+        }
+        User user = userMapper.toModel(requestDto);
+        User savedUser = userRepository.save(user);
+        return userMapper.toDto(savedUser);
+    }
+}
