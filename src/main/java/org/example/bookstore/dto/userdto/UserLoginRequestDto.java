c96bc2c63ccfb5675c57fec1387eb7e88fb3eedc
John <pandastars535@gmail.com>
2024-05-14 03:13:24 +0300

    implemented Token

---
+++ src/main/java/org/example/bookstore/dto/userdto/UserLoginRequestDto.java
@@ -0,0 +1,16 @@
+package org.example.bookstore.dto.userdto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.Length;
+
+public record UserLoginRequestDto(
+        @NotBlank
+        @Email
+        String email,
+        @NotBlank
+        @Length(min = 4,max = 32)
+        String password
+) {
+
+}
