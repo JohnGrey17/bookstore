68b8338423cc8f9eaff39ea9fa0e89be953474e6
John <pandastars535@gmail.com>
2024-05-16 03:29:39 +0300

    Added ChangeLog create-category-table add-category-id-to-books-table add-book_id-to-categories-table add-foreign-keys

--- src/main/java/org/example/bookstore/repository/book/BookRepository.java
+++ src/main/java/org/example/bookstore/repository/book/BookRepository.java
@@ -2,6 +2,8 @@ package org.example.bookstore.repository.book;
 
 import java.util.List;
 import java.util.Optional;
+
+import org.example.bookstore.dto.bookdto.BookResponseDto;
 import org.example.bookstore.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
@@ -11,8 +13,5 @@ import org.springframework.stereotype.Repository;
 public interface BookRepository extends JpaRepository<Book,Long>, JpaSpecificationExecutor<Book> {
 
     Optional<Book> findByIsbn(String isbn);
-
-    List<Book> findAllByCategoryId(Long categoryId);
-
-
+    Optional<BookResponseDto> findBookByCategoriesId(Long categories_id);
 }
