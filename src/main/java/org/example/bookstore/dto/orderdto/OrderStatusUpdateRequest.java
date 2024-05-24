8ec6e6a7c3a6ac31a99954adecb461aa33b881f6
John <pandastars535@gmail.com>
2024-05-24 05:24:21 +0300

    Implemented all methods

---
+++ src/main/java/org/example/bookstore/dto/orderdto/OrderStatusUpdateRequest.java
@@ -0,0 +1,8 @@
+package org.example.bookstore.dto.orderdto;
+
+import lombok.Data;
+
+@Data
+public class OrderStatusUpdateRequest {
+    private String status;
+}
---
+++ src/main/java/org/example/bookstore/dto/orderdto/OrderUpdatedDto.java
@@ -0,0 +1,5 @@
+package org.example.bookstore.dto.orderdto;
+
+public class OrderUpdatedDto {
+
+}
