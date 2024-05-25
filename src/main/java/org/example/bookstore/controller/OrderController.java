af4826257fd9c314a454708ca96dc1dfbba65d20
John <pandastars535@gmail.com>
2024-05-25 19:19:21 +0300

    Fixed

--- src/main/java/org/example/bookstore/controller/OrderController.java
+++ src/main/java/org/example/bookstore/controller/OrderController.java
@@ -2,6 +2,7 @@ package org.example.bookstore.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.example.bookstore.dto.orderdto.OrderRequestDto;
@@ -14,9 +15,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,7 +34,7 @@ public class OrderController {
     @Operation(summary = "Create a new order", description = "Create a new order for "
             + "the authenticated user.")
     public OrderResponseDto createNewOrder(Authentication authentication,
-                                           @RequestBody OrderRequestDto requestDto) {
+                                           @RequestBody @Valid OrderRequestDto requestDto) {
         return orderService.createNewOrder(requestDto, authentication);
     }
 
@@ -62,7 +63,7 @@ public class OrderController {
         return orderService.getItemByOrderIdAndItemId(orderId, itemId);
     }
 
-    @PutMapping(value = "/{orderId}")
+    @PatchMapping(value = "/{orderId}")
     @Operation(summary = "Change order status", description = "Change the status"
             + " of an order by its ID.")
     public OrderUpdatedDto changeStatusOfOrder(@PathVariable Long orderId,
